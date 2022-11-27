7df9d749cf6e7401feb5669e9a9265bbd73b1d8d
progmaster001 <a.rehman5110@gmail.com>
2022-11-27 15:42:28 +0500

    update

--- services/get_counts_values.php
+++ services/get_counts_values.php
@@ -9,12 +9,16 @@ $sql1="select count(*) from tbl_survey_details";
 $sql2="select count(*) from tbl_survey_details where installed_status='0'";
 $sql3="select count(*) from tbl_survey_details where installed_status='Installed'";
 $sql4="select count(*) from tbl_survey_details where installed_status='TRAS'";
+$spl5="select count(*) from tbl_meter where created_at::date = now()::date";
+$spl5="select count(*) from tbl_survey_details where week_no =(select  max(week_no) from tbl_survey_details) and installed_status<>''";
 
 //echo $sql1."<br/>";
 $query1=pg_query($sql1);
 $query2=pg_query($sql2);
 $query3=pg_query($sql3);
 $query4=pg_query($sql4);
+$query5=pg_query($sql5);
+$query6=pg_query($sql6);
 
 
 if($query1)
@@ -32,6 +36,14 @@ if($query3)
 if($query4)
 {
     $output['watt'] = pg_fetch_all($query4);
+}
+if($query5)
+{
+    $output['today'] = pg_fetch_all($query5);
+}
+if($query6)
+{
+    $output['week'] = pg_fetch_all($query6);
 }
 echo  json_encode($output);
 
